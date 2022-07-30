f4bca9e4fb952b70f224c2cc3534a01f098968ed
Tony <ga478267@gmail.com>
2022-07-30 11:44:24 +0800

    新增model controller

---
+++ src/main/java/com/leedong/covidnews/model/Data.java
@@ -0,0 +1,43 @@
+package com.leedong.covidnews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Data {
+    private String explanation;
+    private String name;
+    private String connection;
+    @JsonIgnore
+    private String connectionUrl;
+
+    public String getConnectionUrl() {
+        return connectionUrl;
+    }
+
+    public void setConnectionUrl(String connectionUrl) {
+        this.connectionUrl = connectionUrl;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getConnection() {
+        return connection;
+    }
+
+    public void setConnection(String connection) {
+        this.connection = connection;
+    }
+}
---
+++ src/main/java/com/leedong/covidnews/model/News.java
@@ -0,0 +1,62 @@
+package com.leedong.covidnews.model;
+
+import java.util.Date;
+import java.util.List;
+
+public class News {
+    private String title;
+    private String content;
+    private String connectionUrl;
+    private List<Data> dataList;
+    private Date createdDate;
+    private Date modifiedDate;
+
+    public List<Data> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(List<Data> dataList) {
+        this.dataList = dataList;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getConnectionUrl() {
+        return connectionUrl;
+    }
+
+    public void setConnectionUrl(String connectionUrl) {
+        this.connectionUrl = connectionUrl;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
